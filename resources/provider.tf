e73e310ee953717d7679787cc9f3e750fc2fb4d9
parth-thoriya-prominentpixel <parth.thoriya@prominentpixel.com>
2025-09-30 19:15:35 +0530

    Update provider.tf

--- resources/provider.tf
+++ resources/provider.tf
@@ -30,7 +30,7 @@ terraform {
 # Default
 provider "aws" {
   region     = "us-east-1"
-assume_role {
-    role_arn = var.aws_assume_role_arn != "" ? var.aws_assume_role_arn : null
+  assume_role {
+    role_arn = getenv("TFC_AWS_RUN_ROLE_ARN")
   }
 }
