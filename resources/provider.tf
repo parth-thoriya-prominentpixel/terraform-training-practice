e26cf8a4ca8d4dc1e715344536486d16ba3a37df
parth-thoriya-prominentpixel <parth.thoriya@prominentpixel.com>
2025-09-30 17:29:11 +0530

    Create provider.tf

---
+++ resources/provider.tf
@@ -0,0 +1,33 @@
+terraform {
+#  cloud {
+#    organization = "parth-thoriya-prominentpixel"
+#    workspaces {
+#      name = "terraform-training-practice"
+#    }
+#  }
+terraform {
+  backend "remote" {
+    organization = "parth-thoriya-prominentpixel"
+
+    workspaces {
+      name = "terraform-training-practice"
+    }
+  }
+}
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.0"
+    }
+  }
+
+}
+
+
+
+# Default
+provider "aws" {
+  region     = "us-east-1"
+
+}
